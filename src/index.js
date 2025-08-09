32a28d1ee3fd66b562e98bd2b1fda8f3d539e1c6
Gleb <glebvoronov309@gmail.com>
2025-08-09 12:18:21 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import contactRoutes from './routes/contactRoutes.js';
+import { initMongoConnection } from './db/initMongoConnection.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use('/contacts', contactRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not Found' });
+});
+
+const MONGO_URI = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
+
+const startServer = async () => {
+  await initMongoConnection(MONGO_URI);
+  app.listen(PORT, () => {
+    console.log(` Server running on port ${PORT}`);
+  });
+};
+
+startServer();
